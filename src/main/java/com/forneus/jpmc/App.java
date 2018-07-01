a43a3b6587463236ccd0d1a061c36bc9a6cc18fe
30-Forneus <32784249+30-Forneus@users.noreply.github.com>
2018-07-01 14:35:18 -0300

    first commit

---
+++ src/main/java/com/forneus/jpmc/App.java
@@ -0,0 +1,13 @@
+package com.forneus.jpmc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
